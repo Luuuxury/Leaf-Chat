776fc6353b54e181fdce7d03bbf9e4b59ef15e6d
Titanium-ly <ly610855878@icloud.com>
2022-04-21 13:55:09 +0800

    Add password encryption(bcrypt)

--- Servers/login/internal/handler.go
+++ Servers/login/internal/handler.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/name5566/leaf/gate"
 	"github.com/name5566/leaf/log"
+	"golang.org/x/crypto/bcrypt"
 	"gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
 	"leaf-chat/Servers/db/mongodb"
@@ -48,7 +49,12 @@ func handleRegist(args []interface{}) {
 		return
 	}
 	// 如果该用户名没有被注册过，就直接 insert
-	err = mongodb.Insert("userDB", "regist", bson.M{"name": recv.RegistName, "password": recv.RegistPW})
+	hashpw, err := bcrypt.GenerateFromPassword([]byte(recv.RegistPW), bcrypt.DefaultCost)
+	if err != nil {
+		log.Debug("密码加密过程出错")
+	}
+	strpw := string(hashpw)
+	err = mongodb.Insert("userDB", "regist", bson.M{"name": recv.RegistName, "password": strpw})
 	if err != nil {
 		log.Debug("数据库添加用户名失败!")
 	} else {
@@ -57,31 +63,32 @@ func handleRegist(args []interface{}) {
 }
 
 func handleLogin(args []interface{}) {
-	receMsg := args[0].(*msg.UserLogin)
+	recv := args[0].(*msg.UserLogin)
 	agent := args[1].(gate.Agent)
-	log.Debug("登陆用户名是: %v", receMsg.LoginName)
-	log.Debug("登陆密码是: %v", receMsg.LoginPW)
-	if receMsg.LoginName == "" {
+	// log.Debug("登陆用户名是: %v", recv.LoginName)
+	// log.Debug("登陆密码是: %v", recv.LoginPW)
+	if recv.LoginName == "" {
 		log.Debug("登陆用户名为空!")
 		return
 	}
 
 	// 从数据库核对用户名和密码
-	userData, err := mongodb.FetchUserData(receMsg.LoginName)
+	userData, err := mongodb.FetchUserData(recv.LoginName)
 	// 如果数据库没有这个人，就把把这个人添加进去
 	if err == mgo.ErrNotFound {
-		log.Debug("登陆用户名不存在，请输入正确的用户名")
-	}
-	if err != nil {
-		log.Debug("登陆获取用户信息出错 err: %v", err)
+		log.Debug("登陆用户名不存在，请输入正确的用户名!")
 		return
-		// 如果有这个人，但是密码错误
-	} else if userData.Password != receMsg.LoginPW {
-		log.Debug("登陆密码不对！")
+	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(userData.Password), []byte(recv.LoginPW))
+	if err != nil {
+		log.Debug("输入的用户名或密码不正确!")
+	} else {
+		log.Debug("登陆成功!")
 	}
 
 	// 将该用户加入群聊
-	agent.SetUserData(receMsg.LoginName)
+	agent.SetUserData(recv.LoginName)
 	for i := 0; i < maxMessages; i++ {
 		index := (messageIndex + i) % maxMessages
 		pm := &messages[index]
